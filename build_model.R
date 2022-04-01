10767add90cc6228660e1f108cbb7ab4499d73b5
Valli Subasri <vallisubasri@gmail.com>
2022-03-31 21:45:55 -0400

    Update build_model.R

--- build_model.R
+++ build_model.R
@@ -1,15 +1,15 @@
-suppressWarnings(suppressMessages(require(stringr)))
-suppressWarnings(suppressMessages(require(parallel)))
-suppressWarnings(suppressMessages(require(doParallel)))
-suppressWarnings(suppressMessages(require(dplyr)))
-suppressWarnings(suppressMessages(require(magrittr)))
-suppressWarnings(suppressMessages(require(caret)))
-suppressWarnings(suppressMessages(require(stringr)))
-suppressWarnings(suppressMessages(require(e1071)))
-suppressWarnings(suppressMessages(require(randomForest)))
-suppressWarnings(suppressMessages(require(glmnet)))
-suppressWarnings(suppressMessages(require(Matrix)))
-suppressWarnings(suppressMessages(require(argparse)))
+suppressMessages(require(stringr))
+suppressMessages(require(parallel))
+suppressMessages(require(doParallel))
+suppressMessages(require(dplyr))
+suppressMessages(require(magrittr))
+suppressMessages(require(caret))
+suppressMessages(require(stringr))
+suppressMessages(require(e1071))
+suppressMessages(require(randomForest))
+suppressMessages(require(glmnet))
+suppressMessages(require(Matrix))
+suppressMessages(require(argparse))
 source('models.R')
 source('util.R')
 
@@ -26,16 +26,20 @@ parser$add_argument("-g", "--genes", type="integer", default=1000,
     help="Number of DEGs")
 parser$add_argument("-s", "--size", type="integer", default=1000,
     help="Number of each cells for each cell type to include in the training set")
-parser$add_argument("-m", "--model", type="integer", default=1000,
+parser$add_argument("-m", "--model", type="character", default="rf",
     help="Model to train for cell type classficiation")
 parser$add_argument("--i", "--id", type="character",  
     help = "Prefix for filename")
 parser$add_argument("--d", "--data_dir", type="character",  
-    help = "Output directory for models")
+    help = "Directory that contains preprocessed data")
 parser$add_argument("--r", "--results_dir", type="character",  
     help = "Output directory for models")
 
 args <- parser$parse_args()
+
+# -------------------------------------
+# parse variables
+# -------------------------------------
 
 genes <- args$genes
 size <- args$size
@@ -45,12 +49,16 @@ results_dir <- args$results_dir
 model <- args$model
 
 # -------------------------------------
-# specify paths and load functions
+# load data 
 # -------------------------------------
 
 data <- load_data(data_dir,id,genes,size)
 x_train <- data[[1]] ; y_train <- data[[2]]
 x_test <- data[[3]] ; y_test <- data[[4]]
+
+# -------------------------------------
+# fit model
+# -------------------------------------
 
 if (model == "rf_default") {
     rf_default(results_dir,id,genes,size,model,x_train,y_train,x_test)
